5566820c1e65424443bae0b63223a5aa11b13f4e
MakerSquare Student <students@themakersquare.com>
2013-10-11 15:37:57 -0500

    adding user show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   belongs_to :phase
   has_many :posts
   default_scope order('created_at DESC')
-  before_save :set_phase_to_onboarding
+  before_create :set_phase_to_onboarding
 
   def set_phase_to_onboarding
     self.phase_id = Phase.first.id
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Ativa::Application.routes.draw do
   resources :posts
-
   root 'projects#index'
   resources :projects
+
+  get 'users/:id' => 'users#show', as: 'user'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
