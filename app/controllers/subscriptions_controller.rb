1f0c42bc4afe89ff223c30f478ec0c1ec5de73c7
Ricardo Cervantes <ricardo07@gmail.com>
2013-10-15 18:13:42 -0500

    Add set_project to Subscriptions Controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,14 +1,15 @@
 class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:destroy]
+  before_action :set_project
 
   def create
     @subscription = current_user.subscriptions.build(project_id: params[:project_id])
 
     respond_to do |format|
       if @subscription.save
-        format.html { redirect_to project_path(params[:project_id]), notice: 'You are now subscribed to this project.' }
+        format.html { redirect_to project_path(@project.id), notice: 'You are now subscribed to this project.' }
       else
-        format.html { render action: 'show', alert: 'Your subscription could not be processed.' }
+        format.html { redirect_to project_path(@project.id), alert: 'Your subscription could not be processed.' }
       end
     end
   end
@@ -16,7 +17,7 @@ class SubscriptionsController < ApplicationController
   def destroy
     @subscription.destroy
     respond_to do |format|
-      format.html { redirect_to project_path(params[:project_id]), notice: 'You are now unsubscribed from this project.' }
+      format.html { redirect_to project_path(@project.id), notice: 'You are now unsubscribed from this project.' }
       format.json { head :no_content }
     end
   end
@@ -25,5 +26,9 @@ class SubscriptionsController < ApplicationController
 
     def set_subscription
       @subscription = Subscription.find(params[:id])
+    end
+
+    def set_project
+      @project = Project.find(params[:project_id])
     end
 end
