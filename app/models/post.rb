f70634279df57d214160db86ee57705b7b94bd75
Ricardo Cervantes <ricardo07@gmail.com>
2013-10-15 21:32:02 -0500

    Remove unnecessary comment from post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,6 @@ class Post < ActiveRecord::Base
     def set_defaults
       if self.title == "" then self.title = "Post #{rand(255)}" end
       if self.message == "" then self.message = "(no message)" end
-      # @attachment ||= "https://s3.amazonaws.com/mks_learn_app/mks-startupcrawl-card-front.png"
     end
 
 end
