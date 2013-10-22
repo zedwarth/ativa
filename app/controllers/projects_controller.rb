5480384298c3ad77f8912754f47ef3b020542040
Michael Ornellas <mike@themakersquare.com>
2013-10-22 07:51:03 -0500

    bork projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    # @projects = Project.all
     @project = Project.new
 
   end
