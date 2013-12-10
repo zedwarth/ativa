21e982beac4e74a6dfadde19e3765efb6708eadf
Casey Lang <clang1234@gmail.com>
2013-12-10 15:23:31 -0600

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Project.destroy_all
 Phase.destroy_all
 Post.destroy_all
 User.destroy_all
 
 users = [
-  {name: "Batman", email: 'batman@themakersquare.com', profile_pic: 'https://s3.amazonaws.com/mks_learn_app/batman.jpg'},
-  {name: "Catwoman", email: 'catwoman@themakersquare.com', profile_pic: 'https://s3.amazonaws.com/mks_learn_app/catwoman.jpg'}
+  {name: "Batman", email: 'batman@example.com', profile_pic: 'https://s3.amazonaws.com/mks_learn_app/batman.jpg', password: 'DickGrayson4Life'},
+  {name: "Catwoman", email: 'catwoman@example.com', profile_pic: 'https://s3.amazonaws.com/mks_learn_app/catwoman.jpg', password: 'I<3Bruce'}
 ]
 
 users.each do |user|
   User.create(
     name: user[:name],
     email: user[:email],
-    password: 'password',
+    password: user[:password],
     profile_pic: user[:profile_pic]
   )
 end
 
-phases = ['onboarding','creative', 'design', 'development', 'implementation']
+phases = ['onboarding', 'creative', 'design', 'development', 'implementation']
 
 phases.each do |phase|
   Phase.create(name: phase)
@@ -36,7 +28,7 @@ projects = [
   'GameStarter',
   'Prepify',
   'Mission Impossible',
-  'Humppp Day',
+  'Clownstagram',
   'FreqShow',
   'MikeMikeMike',
   'iDiningDeals',
@@ -67,14 +59,9 @@ posts = [
     title: "Schedule",
     message: "We have strict schedule outlined for the project. It should take 3 months in total if we stay on pace. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.",
     attachment: "https://ativa.s3.amazonaws.com/uploads/attachment/file/62/PSD_Calendar-_iDiningDeals.pdf"
-  },
-  {
-    title: "Create Sign In Page",
-    message: "Create a sign in page that allows native sign in or the use of Facebook or Twitter. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.",
-    attachment: "https://ativa.s3.amazonaws.com/uploads/attachment/file/88/1_Home_with_Sign_In.pdf"
   }
-
 ]
+
 Project.all.each do |project|
   Phase.all.each do |phase|
     posts.each do |post|
