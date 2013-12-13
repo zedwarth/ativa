a601c4e16dfdb0b0748c25314f4c21b82398314e
Casey Lang <clang1234@gmail.com>
2013-12-12 22:47:52 -0600

    Fix Post update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_instance_variables, only: [:show, :edit, :update, :destroy]
+  before_action :set_instance_variables, only: [:show, :edit]
 
   def show
   end
@@ -28,9 +28,10 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to project_post_path(@post.project_id, @post, phase_id: @post.phase_id), notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -57,6 +58,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :message, :attachment, :phase_id, :project_id)
+      params.require(:post).permit(:title, :message, :attachment, :phase_id, :project_id, :user_id)
     end
 end
